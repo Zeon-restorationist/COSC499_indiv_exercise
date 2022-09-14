5a38a4a915f7ab6ca52bc93fee406ed64fa1604b
Zeon-restorationist <99059238+Zeon-restorationist@users.noreply.github.com>
2022-09-14 13:17:56 -0700

    Class skeleton created

--- Console/src/main/java/main.java
+++ Console/src/main/java/main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+
 public class main {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for (int i = 0; i < 5; i++) {
+            a.add(i);
+        }
+
+        System.out.println(bubSort(a));
+        System.out.println(insertSort(a));
+    }
+
+
+
+
+
+    public static ArrayList<Integer> bubSort(ArrayList in){
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        return temp;
+    }
+    public static ArrayList<Integer> insertSort(ArrayList in){
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        return temp;
     }
 }
