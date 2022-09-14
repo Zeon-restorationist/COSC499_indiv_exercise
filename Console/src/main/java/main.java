556cb635297bbe1b91f44fa61ce4e9ddac6ce8af
Zeon-restorationist <99059238+Zeon-restorationist@users.noreply.github.com>
2022-09-14 15:23:01 -0700

    update bubble sort function

--- Console/src/main/java/main.java
+++ Console/src/main/java/main.java
@@ -19,7 +19,24 @@ public class main {
 
 
     public static ArrayList<Integer> bubSort(ArrayList in){
-        ArrayList<Integer> temp = new ArrayList<Integer>();
+        ArrayList<Integer> temp = in;
+        int hold;
+        int count = 0;
+        boolean sorted = false;
+        while(sorted!= true){
+            for (int i = 0; i < temp.size()-1; i++) {
+                if(temp.get(i) > temp.get(i+1)){
+                    hold = temp.get(i+1);
+                    temp.set(i+1, temp.get(i));
+                    temp.set(i, hold);
+                    count ++;
+                }
+            }
+            if(count == 0){
+                break;
+            }
+            count = 0;
+        }
         return temp;
     }
     public static ArrayList<Integer> insertSort(ArrayList in){
