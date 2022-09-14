9ad1f2e9c53088ee21f792be18dfdf96181c8fec
Zeon-restorationist <99059238+Zeon-restorationist@users.noreply.github.com>
2022-09-14 13:33:27 -0700

    main revision

--- Console/src/main/java/main.java
+++ Console/src/main/java/main.java
@@ -4,10 +4,13 @@ public class main {
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<Integer>();
         for (int i = 0; i < 5; i++) {
-            a.add(i);
+            a.add((int) (Math.random()*10));
         }
-
+        System.out.println("Input array: ");
+        System.out.println(a);
+        System.out.println("Bubble sorted: ");
         System.out.println(bubSort(a));
+        System.out.println("Insertion sorted: ");
         System.out.println(insertSort(a));
     }
 
