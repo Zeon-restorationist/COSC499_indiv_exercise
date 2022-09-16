7ead0b6e46b785212e43b7a8d08b422603d1ecb5
Zeon-restorationist <99059238+Zeon-restorationist@users.noreply.github.com>
2022-09-15 22:16:40 -0700

    added test for bubble sort

--- Console/src/main/java/main.java
+++ Console/src/main/java/main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class main {
+<<<<<<< Updated upstream
     public static void main(String[] args) {
         //Instantiating input arraylist
         ArrayList<Integer> a = new ArrayList<Integer>();
@@ -20,12 +21,20 @@ public class main {
         //Calls insertSort() with "a" as input
         System.out.println(insertSort(a));
     }
+=======
+>>>>>>> Stashed changes
 
 
 
 
+<<<<<<< Updated upstream
     //Bubble sort function, requires one Arraylist of Int type and returns a sorted arraylist.
     public static ArrayList<Integer> bubSort(ArrayList in){
+=======
+
+    //Bubble sort function, requires one Arraylist of Int type and returns a sorted arraylist.
+    public static ArrayList <Integer> bubSort(ArrayList in){
+>>>>>>> Stashed changes
         ArrayList<Integer> temp = in;
         int hold;
         int count = 0;
@@ -47,8 +56,34 @@ public class main {
         return temp;
     }
     //Insert sort function, requires one Arraylist of Int type and returns a sorted arraylist.
+<<<<<<< Updated upstream
     public static ArrayList<Integer> insertSort(ArrayList in){x
         ArrayList<Integer> temp = new ArrayList<Integer>();
+=======
+    /*
+    public static ArrayList<Integer> insertSort(ArrayList in){
+
+        ArrayList<Integer> temp = in;
+
+        int i, key, j;
+        for (i = 1; i < temp.size(); i++)
+        {
+            key = temp.get(i);
+            j = i - 1;
+
+            // Move elements of arr[0..i-1],
+            // that are greater than key, to one
+            // position ahead of their
+            // current position
+            while (j >= 0 && temp.get(j) > key)
+            {
+                temp.set(j+1, temp.get(j));
+                j -= 1;
+            }
+            temp.set(j+1, key);
+        }
+>>>>>>> Stashed changes
         return temp;
     }
+    */
 }
---
+++ Console/src/test/java/test.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class test {
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+    @Test
+    public void testbubble(){
+        //Initialize input arraylist
+        ArrayList<Integer> a = new ArrayList<Integer>(){{
+            add(3);
+            add(4);
+            add(1);
+            add(5);
+            add(2);
+        }};
+
+        //prints unsorted arraylist
+        System.out.println("Input array: ");
+        System.out.println(a);
+        //prints bubble sorted arraylist
+        System.out.println("Bubble sorted: ");
+        //Calls bubSort() with "a" as input
+        System.out.println(main.bubSort(a));
+
+    }
+
+    @Test
+    public void testSelection(){
+        //Initialize input arraylist
+        ArrayList<Integer> a = new ArrayList<Integer>(){{
+            add(3);
+            add(4);
+            add(1);
+            add(5);
+            add(2);
+        }};
+        //prints unsorted arraylist
+        System.out.println("Input array: ");
+        System.out.println(a);
+        //prints insertion sorted arraylist
+        System.out.println("Insertion sorted: ");
+        //Calls insertSort() with "a" as input
+        System.out.println(main.insertSort(a));
+    }
+}
